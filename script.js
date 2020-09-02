b199ab30b6ee1ba80ab3c1f485aea9c638d636f2
Haris Nazir <haris.nazir1963@gmail.com>
2020-09-02 20:32:44 +0100

    loaded models

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const video = document.getElementById("video");
 
+Promise.all(
+    [
+        faceapi.nets.tnyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.tnyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+    ]
+)
+
 function startVideo(){
     navigator.getUserMedia(
         { video: {} },
